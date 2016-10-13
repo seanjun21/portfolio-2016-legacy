717908327a3e7b38f3275712d8e42c2855ad90ac
Sean Jun <seanjun21@gmail.com>
2016-10-13 15:40:52 -0700

    navbar updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    $('#masthead-nav').css({'background': '#0f2a56'});
-    $('#about-nav').css({'background': '#9c1737'});
-    $('#projects-nav').css({'background': '#9c1737'});
-    $('#masthead-nav a').css({'color': 'white'});
-    $('#about-nav a').css({'color': 'white'});
-    $('#projects-nav a').css({'color': 'white'});
 
     var aboutOffset = $('#about').position().top * 0.94;
     var projectsOffset = $('#projects').position().top * 0.97;
@@ -13,28 +7,19 @@ $(document).ready(function () {
         var scrollPos = $(window).scrollTop();
 
         if (scrollPos < aboutOffset) {
-            $('#masthead-nav').css({'background': '#0f2a56'});
-            $('#about-nav').css({'background': '#9c1737'});
-            $('#projects-nav').css({'background': '#9c1737'});
-            $('#masthead-nav a').css({'color': 'white'});
-            $('#about-nav a').css({'color': 'white'});
-            $('#projects-nav a').css({'color': 'white'});
+            $('#masthead-nav a').css({'background': '#0f2a56', 'color': 'white'});
+            $('#about-nav a').css({'background': '#9c1737', 'color': 'white'});
+            $('#projects-nav a').css({'background': '#9c1737', 'color': 'white'});
         }
         else if (scrollPos >= aboutOffset && scrollPos < projectsOffset) {
-            $('#masthead-nav').css({'background': '#9c1737'});
-            $('#about-nav').css({'background': 'white'});
-            $('#projects-nav').css({'background': '#9c1737'});
-            $('#masthead-nav a').css({'color': 'white'});
-            $('#about-nav a').css({'color': 'black'});
-            $('#projects-nav a').css({'color': 'white'});
+            $('#masthead-nav a').css({'background': '#9c1737', 'color': 'white'});
+            $('#about-nav a').css({'background': 'white', 'color': 'black'});
+            $('#projects-nav a').css({'background': '#9c1737', 'color': 'white'});
         }
         else {
-            $('#masthead-nav').css({'background': '#9c1737'});
-            $('#about-nav').css({'background': '#9c1737'});
-            $('#projects-nav').css({'background': '#ffcb17'});
-            $('#masthead-nav a').css({'color': 'white'});
-            $('#about-nav a').css({'color': 'white'});
-            $('#projects-nav a').css({'color': 'black'});
+            $('#masthead-nav a').css({'background': '#9c1737', 'color': 'white'});
+            $('#about-nav a').css({'background': '#9c1737', 'color': 'white'});
+            $('#projects-nav a').css({'background': '#ffcb17', 'color': 'black'});
         }
     });
 });
